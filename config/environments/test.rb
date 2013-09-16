ced85d179cdc1cf161be380e4589231fc85de92e
KazukiYunoue <you.know.ue1480@gmail.com>
2013-09-16 12:10:00 +0900

    set some libraries.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Momently::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Use devise's email address.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
